9b1d219a1d9c29f6fe3d9206ffc9e68e8c98ae20
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-05-13 13:03:35 +0100

    task 0  Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,53 @@
+import redis from 'redis';
+
+class RedisClient {
+  constructor() {
+    this._client = redis.createClient();
+    this._client.on('error', (error) => {
+      console.log(error);
+    });
+  }
+
+  isAlive() {
+    try {
+      this._client.ping();
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this._client.get(key, (err, value) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(value);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this._client.set(key, value, 'EX', duration);
+      resolve(value);
+      this._client.on('error', (error) => {
+        reject(error);
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      resolve(this._client.del(key));
+      this._client.on('error', (error) => {
+        reject(error);
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
