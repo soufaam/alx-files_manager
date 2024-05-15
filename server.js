87e1f136395b396d8ea32a0eea0f6c28b0890f0e
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-05-15 16:30:12 +0100

    Create a new user ==> fix lint style

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use('/', endpoints);
 
-
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
